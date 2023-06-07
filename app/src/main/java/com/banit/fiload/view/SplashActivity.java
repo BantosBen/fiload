6ebca7bb3ed18291cedd54bdb94b64d8c380fb29
Angatia Benson <angatiabenson1@gmail.con>
2023-06-07 03:20:24 -0500

    feat: complete splash screen impl

--- app/src/main/java/com/banit/fiload/view/SplashActivity.java
+++ app/src/main/java/com/banit/fiload/view/SplashActivity.java
@@ -2,7 +2,9 @@ package com.banit.fiload.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.banit.fiload.R;
 
@@ -12,5 +14,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            // Create an Intent to navigate to the next activity
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // This prevents going back to the splash screen when pressing the back button
+        }, 2000); // Delay in milliseconds (e.g., 2000 = 2 seconds)
+
     }
 }
